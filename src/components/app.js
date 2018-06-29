f9cd6c99c000a1bad127ff497931f0f972d5e59f
Luis Miranda <amp069597@gmail.com>
2018-06-29 15:01:48 -0600

    pushing up before switching devices

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
+import changeDate from './changeDate';
 
 export default class App extends Component {
 
@@ -18,6 +19,7 @@ export default class App extends Component {
     if (this.state.active) {
       return [
         <Clock />,
+        changeDate('Change Date', () => this.setState({ active: false }))
 
       ]
     } else {
