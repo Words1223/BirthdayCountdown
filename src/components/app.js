5725c4e51d3c8dd0b0765f0597ed10b0d619e53f
Words1223 <amp069597@gmail.com>
2018-07-02 03:17:41 -0600

    added age calculation

--- src/components/app.js
+++ src/components/app.js
@@ -22,7 +22,8 @@ export default class App extends Component {
         hours: 0,
         minutes: 0,
         seconds: 0
-      }
+      },
+      age: 0
     }
 
     this.handleGenerate= this.handleGenerate.bind(this)
@@ -37,12 +38,20 @@ export default class App extends Component {
 }
 
   handleGenerate = function() {
-    this.setState({ active: true });
 
   var bday = this.state.startDate.toDate().getTime();
   var today = new Date();
   var currentMonth = today.getMonth();
   var birthMonth = bday.getMonth();
+
+  var timeBetween = today.getTime() - bday.getTime();
+  var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 *24))
+  var age = Number((daysOld/365).toFixed(0));
+  this.setState({ 
+      age, 
+      active: true 
+  })
+    
 
   if(birthMonth > currentMonth) {
     bday.setFullYear(today.getFullYear())
@@ -96,7 +105,9 @@ export default class App extends Component {
         <Clock timeRemaining={this.state.timeRemaining} />,
         changeDate('Change Date', () => this.setState({ active: false })),
         largeText('04/03'),
-        <label className="grid__remaining until your 21at birthday"></label>
+        <label className="grid__remaining">
+          Remaining until you turn {this.state.age}
+        </label>
 
       ]
     } else {
